bafe4f7dd6053c53726913f1f19f67f87ae8c350
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-17 08:18:56 +0400

    very easy

---
+++ ConsoleApp1/Grup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Grup
+    {
+        private string _no;
+        private int _studenlimit;
+        public int StudenLimit
+        {
+            get
+            {
+                return _studenlimit;
+            }
+            set
+            {
+                _studenlimit = value;
+
+            }
+        }
+        public string No
+        {
+            get
+            {
+                return this._no;
+            }
+            set
+            {
+                if (hasUpper(value) && HasDigit(value))
+                {
+                   _no = value; 
+                }
+                 
+            }
+        }
+        public bool HasDigit(string text)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (Char.IsDigit(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+        public bool hasUpper(string text)
+        {
+            for (int i = 3; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            Grup grups = new Grup();
+            Console.WriteLine("enter the student count :");
+            grups.StudenLimit = (Convert.ToInt32(Console.ReadLine()));
+            if (grups.StudenLimit > 30)
+            {
+                while (grups.StudenLimit > 30)
+                {
+                    Console.WriteLine("enter the student count :");
+                    grups.StudenLimit = (Convert.ToInt32(Console.ReadLine()));
+                }
+            }
+            else
+            {
+                Console.WriteLine(grups.StudenLimit);
+                Console.ReadLine();
+            }
+            #endregion
+            Grup grupss = new Grup();
+            Console.WriteLine(" enter the NO");
+            grupss.No = Console.ReadLine();
+            if (grupss.No.Length != 5)
+            {
+                while (grupss.No.Length == 5)
+                {
+                    Console.WriteLine(" enter the NO");
+                    grupss.No = Console.ReadLine();
+                }
+            }
+            else if (grupss.No.Length == 5)
+            {
+                Console.WriteLine(grupss.No);
+            }  
+
+        }
+    }
+}
